27d12c2cd8960c97571241b3519861966c5cbe09
bhgubhgu <bbhgu@naver.com>
2018-07-02 12:22:01 +0900

    dev_용모 2018.07.02
    Test용 플레이어 인벤토리, 상점 인벤토리 코드 및 프리펩
    
    플레이에 영향을 주는 하이러라키창에 있는 etc 오브젝트를 수정해서 프리펩화 시킴

---
+++ dev_용모 Test용 인벤토리 코드 및 prefab/TestTradeSystem.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestTradeSystem : SingleTonManager<TestTradeSystem>
+{
+    private GameObject playerInventory;
+    private GameObject shopInventory;
+
+    private Dictionary<Sprite, float> shopObjectDictionary;
+
+    private new void Awake()
+    {
+        base.Awake();
+        shopObjectDictionary = new Dictionary<Sprite, float>();
+        playerInventory = GameObject.Find("Player Inventory").gameObject;
+        shopInventory = GameObject.Find("Shop Inventory").gameObject;
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < shopInventory.transform.childCount; i++)
+        {
+            shopObjectDictionary.Add(shopInventory.transform.GetChild(i).transform.GetChild(0).gameObject.GetComponent<Image>().sprite, 500f);
+        }
+    }
+
+    public void Trade(Sprite tradesprite, float checkObject)
+    {
+        if(checkObject == 0) //Player Sell
+        {
+            if(tradesprite == null)
+            {
+                return;
+            }
+            else
+            {
+                TestMoneyCheck.money += shopObjectDictionary[tradesprite];
+            }
+        }
+        else //Player Buy
+        {
+            if (tradesprite == null)
+            {
+                return;
+            }
+            else
+            {
+                if(TestMoneyCheck.money <= 0.0f)
+                {
+                    return;
+                }
+                else
+                {
+                    TestMoneyCheck.money -= shopObjectDictionary[tradesprite];
+                }
+            }
+        }
+    }
+}
