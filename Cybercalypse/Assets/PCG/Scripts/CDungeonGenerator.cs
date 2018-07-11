3a0726b9cbccc227c8fc3f6c7de29413848dce9a
Zaoxi <whgmlwo1000@gmail.com>
2018-07-11 21:25:09 +0900

    Ground 패턴, Wall 패턴, Ceiling 패턴 게임 오브젝트 생성 알고리즘 구현
    - 각각의 오브젝트가 생성될 수 있는 패턴을 구상하여 게임 오브젝트가 생성되는 알고리즘을 구현
    
    - 쓸모없는 공간이나 겜이 요소에 필요한 공간을 생성하는 패턴도 필요

--- Cybercalypse/Assets/PCG/Scripts/CDungeonGenerator.cs
+++ Cybercalypse/Assets/PCG/Scripts/CDungeonGenerator.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class CDungeonGenerator : AContentsGenerator {
 
+    // 플레이어가 다닐 수 있는 가상 공간좌표
     private Dictionary<Vector2Int, ETileType> tileDict;
+    // 게임 오브젝트가 추가된 가상 공간좌표
+    private Dictionary<Vector2Int, ETileType> objectTileDict;
     private Transform tileHolder;
     private float tileLength;
 
@@ -12,6 +15,7 @@ public class CDungeonGenerator : AContentsGenerator {
     {
         base.Awake();
         tileHolder = new GameObject("tileHolder").transform;
+        objectTileDict = new Dictionary<Vector2Int, ETileType>();
     }
 
     public override void GenerateContents()
@@ -19,7 +23,9 @@ public class CDungeonGenerator : AContentsGenerator {
         tileDict = LevelManager.instance.GridGenerator.TileDict;
         tileLength = LevelManager.instance.GridGenerator.TILE_LENGTH;
 
-        generateGameObject_test();
+        //generateGameObject_test();
+        addTileType();
+        generateGameObject();
     }
 
     private void generateGameObject_test()
@@ -35,15 +41,80 @@ public class CDungeonGenerator : AContentsGenerator {
     {
         foreach(KeyValuePair<Vector2Int, ETileType> tile in tileDict)
         {
-            
+            // Ground의 경우
+            if(!objectTileDict.ContainsKey(new Vector2Int(tile.Key.x, tile.Key.y - 1)) &&  // 겹치는것을 방지
+                !tileDict.ContainsKey(new Vector2Int(tile.Key.x, tile.Key.y - 1)))
+            {
+                objectTileDict.Add(new Vector2Int(tile.Key.x, tile.Key.y - 1), ETileType.Ground);
+            }
+
+            // Ceiling의 경우
+            if(!objectTileDict.ContainsKey(new Vector2Int(tile.Key.x, tile.Key.y + 1)) &&
+                !tileDict.ContainsKey(new Vector2Int(tile.Key.x, tile.Key.y + 1)) && 
+                !tileDict.ContainsKey(new Vector2Int(tile.Key.x, tile.Key.y + 2)))
+            {
+                objectTileDict.Add(new Vector2Int(tile.Key.x, tile.Key.y + 1), ETileType.Ceiling);
+            }
+
+            // LeftWall의 경우
+            if(!objectTileDict.ContainsKey(new Vector2Int(tile.Key.x - 1, tile.Key.y)) &&
+                !tileDict.ContainsKey(new Vector2Int(tile.Key.x - 1, tile.Key.y)) &&
+                !tileDict.ContainsKey(new Vector2Int(tile.Key.x - 1, tile.Key.y + 1)))
+            {
+                objectTileDict.Add(new Vector2Int(tile.Key.x - 1, tile.Key.y), ETileType.LeftWall);
+            }
+
+            // RightWall의 경우
+            if(!objectTileDict.ContainsKey(new Vector2Int(tile.Key.x + 1, tile.Key.y)) &&
+                !tileDict.ContainsKey(new Vector2Int(tile.Key.x + 1, tile.Key.y)) && 
+                !tileDict.ContainsKey(new Vector2Int(tile.Key.x + 1, tile.Key.y + 1)))
+            {
+                objectTileDict.Add(new Vector2Int(tile.Key.x + 1, tile.Key.y), ETileType.RightWall);
+            }
+        } // Ceiling과 Ground가, LeftWall과 RIghtWall이 겹치는 문제가 발생할 수 있음
+
+        // objectTileDict의 정보를 tileDict에 전달
+        foreach(KeyValuePair<Vector2Int, ETileType> tile in objectTileDict)
+        {
+            tileDict.Add(tile.Key, tile.Value);
         }
     }
 
     private void generateGameObject()
     {
+        List<CContent> selectedList;
+        GameObject instObject;
+
         foreach (KeyValuePair<Vector2Int, ETileType> tile in tileDict)
         {
-
+            if(tile.Value == ETileType.Ground)
+            {
+                selectedList = elementDict[ETileType.Ground];
+                instObject = GameObject.Instantiate(selectedList[(int)Random.Range(0.0f, selectedList.Count)].content, 
+                    new Vector3(tile.Key.x * tileLength, tile.Key.y * tileLength, 0.0f), Quaternion.identity);
+                instObject.transform.SetParent(tileHolder);
+            }
+            else if(tile.Value == ETileType.Ceiling)
+            {
+                selectedList = elementDict[ETileType.Ceiling];
+                instObject = GameObject.Instantiate(selectedList[(int)Random.Range(0.0f, selectedList.Count)].content,
+                    new Vector3(tile.Key.x * tileLength, tile.Key.y * tileLength, 0.0f), Quaternion.identity);
+                instObject.transform.SetParent(tileHolder);
+            }
+            else if(tile.Value == ETileType.LeftWall)
+            {
+                selectedList = elementDict[ETileType.LeftWall];
+                instObject = GameObject.Instantiate(selectedList[(int)Random.Range(0.0f, selectedList.Count)].content,
+                    new Vector3(tile.Key.x * tileLength, tile.Key.y * tileLength, 0.0f), Quaternion.identity);
+                instObject.transform.SetParent(tileHolder);
+            }
+            else if(tile.Value == ETileType.RightWall)
+            {
+                selectedList = elementDict[ETileType.RightWall];
+                instObject = GameObject.Instantiate(selectedList[(int)Random.Range(0.0f, selectedList.Count)].content,
+                    new Vector3(tile.Key.x * tileLength, tile.Key.y * tileLength, 0.0f), Quaternion.identity);
+                instObject.transform.SetParent(tileHolder);
+            }
         }
     }
 }
