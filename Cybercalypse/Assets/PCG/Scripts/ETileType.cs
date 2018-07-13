2408256762022c190ff6af71afd2933e89d56761
Zaoxi <whgmlwo1000@gmail.com>
2018-07-13 19:12:30 +0900

    2x2 게임 오브젝트 생성 클래스 구조 확장 및 오브젝트 생성 패턴 일부 구현
    - 게임 오브젝트 생성 클래스 구조 확장
    - 2x2 게임 오브젝트 생성 패턴 일부 구현

--- Cybercalypse/Assets/PCG/Scripts/ETileType.cs
+++ Cybercalypse/Assets/PCG/Scripts/ETileType.cs
@@ -3,8 +3,8 @@
 // 생성할 타입의 종류
 public enum ETileType
 {
-    Ground, Platform, RightHill, LeftHill, Ceiling, LeftWall, RightWall, RightStair, LeftStair, Stuff, Empty,
+    Ground, GroundCeiling, GroundBox, Platform, RightHill_0, RightHill_1, LeftHill_0, LeftHill_1, Ceiling, LeftCeiling, RightCeiling,
+    BothWall, LeftWall, RightWall, RightStair, LeftStair, Stuff,
+    Empty, // 타일 타입 구분점
     Portal, MonsterSponer, PlayerSponer, ItemSponer, Decorator, Water, COUNT
 }
-// 평지, 발판, 오르막길, 내리막길, 천장, 왼쪽 벽, 오른쪽 벽, 비었지가 갈 수 없는 공간, 플레이어가 이동할 수 있는 빈 공간,
-// 포탈, 몬스터 스포너, 플레이어 스포너, 아이템 스포너, 장식품, 물, 총 개수
