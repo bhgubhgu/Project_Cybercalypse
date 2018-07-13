70119ab6d952c11e3cf9906b7ea2919ad5d8270e
Totoriche <dbsehdwns1@gmail.com>
2018-07-13 19:11:49 +0900

    dev_동준
    Fork 방식으로 변경하기 위한 합병

---
+++ Cybercalypse/Assets/Cybercalypse/Scripts/Dev_YDJ/CSkill.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSkill : ASkill {
+    public override string ItemName { get; set; }
+    public override string ItemDesc { get; set; }
+    public override Sprite ItemIcon { get; set; }
+    public override Sprite ItemSubs { get; set; }
+    public override EItemCategory ItemCategory { get; set; }
+    public override ETalentCategory TalentCategory { get; set; }
+    public override float SkillCastingTime { get; set; }
+    public override float SkillCoolDown { get; set; }
+}
---
+++ Cybercalypse/Assets/Cybercalypse/Scripts/Dev_YDJ/Shop/CSlotControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSlotControl : MonoBehaviour {
+
+    public List<GameObject> Slots;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Slots.Add(transform.GetChild(i).gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    public List<GameObject> GetListRef()
+    {
+        return Slots;
+    }
+
+    public static GameObject FindEmptySlot(List<GameObject> Slots)
+    {
+        foreach (GameObject item in Slots)
+        {
+            //!< item이 비어있는지 확인한다 -> Image의 Sprite가 null이라면 비어있는 것
+            if (item.GetComponent<UnityEngine.UI.Image>().sprite.Equals(null))
+            {
+                return item;    //!< 빈 슬롯이 존재한다
+            }
+        }
+        return null;    //!< 빈 슬롯이 존재하지 않는다
+    }
+}
